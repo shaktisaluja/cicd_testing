213bb56ce5a11db46dc96e380909bbfd3e2131d6
shaktisaluja <102968268+shaktisaluja@users.noreply.github.com>
2024-11-08 17:36:16 +0530

    added test route

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.use("/api/test", (req, res) => {
   res.send({ message: "server is running" });
 });
 
+app.use("/test", (req, res) => {
+  res.send({ message: "server is running with test" });
+});
+
 
 const server = app.listen(8000, () => {
   console.log(`Server is running on ${8000}`);
